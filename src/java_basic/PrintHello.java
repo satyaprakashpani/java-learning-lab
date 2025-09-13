a2f008725e51f2e3c9976e542a11f2ef8ae907dc
Satyaprakash <satyaprakashpani@gmail.com>
2025-09-13 12:27:28 +0530

    Initial project Commit

---
+++ src/java_basic/PrintHello.java
@@ -0,0 +1,11 @@
+package java_basic;
+
+
+
+public class PrintHello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Jay Jagannath");
+        System.out.println("India is the Best Country");
+    }
+}
