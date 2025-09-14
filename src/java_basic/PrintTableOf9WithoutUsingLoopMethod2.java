bd5c7b5f7c35ec5b1f044697869f343d18efa6da
Satyaprakash <satyaprakashpani@gmail.com>
2025-09-14 15:17:01 +0530

    Print The Table  Of 9 Without Using Loop Method 2

---
+++ src/java_basic/PrintTableOf9WithoutUsingLoopMethod2.java
@@ -0,0 +1,18 @@
+package java_basic;
+
+public class PrintTableOf9WithoutUsingLoopMethod2 {
+    public static void main(String[] args) {
+        int table =9, multiplier = 1,counter=1;
+
+        System.out.printf("%d X %d = %d%n",table,counter++,(multiplier++*table));
+        System.out.printf("%d X %d = %d%n",table,counter++,(multiplier++*table));
+        System.out.printf("%d X %d = %d%n",table,counter++,(multiplier++*table));
+        System.out.printf("%d X %d = %d%n",table,counter++,(multiplier++*table));
+        System.out.printf("%d X %d = %d%n",table,counter++,(multiplier++*table));
+        System.out.printf("%d X %d = %d%n",table,counter++,(multiplier++*table));
+        System.out.printf("%d X %d = %d%n",table,counter++,(multiplier++*table));
+        System.out.printf("%d X %d = %d%n",table,counter++,(multiplier++*table));
+        System.out.printf("%d X %d = %d%n",table,counter++,(multiplier++*table));
+        System.out.printf("%d X %d = %d%n",table,counter++,(multiplier++*table));
+    }
+}
